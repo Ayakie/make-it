9bbf63c815e140923e6accb6e8b05ecba6a41640
Ayakie <ayalake156@gmail.com>
2021-11-13 21:32:43 +0900

    [add] date-fns

--- src/components/task/SingleTask.vue
+++ src/components/task/SingleTask.vue
@@ -2,7 +2,7 @@
   <div class="task">
     <div class="task-container">
       <div class="text">
-        <p class="created-time">{{ doc.createdAt }}</p>
+        <p class="created-time">{{ time }}</p>
         <p class="detail">{{ doc.task }}</p>
       </div>
       <!-- icons -->
@@ -14,10 +14,17 @@
 </template>
 
 <script>
+import { computed, ref } from '@vue/reactivity'
+import { format } from 'date-fns'
+
 export default {
     props: ['doc'],
     setup(props, context) {
-        
+      const time = computed(() => {
+        return format(props.doc.createdAt.toDate(), 'yyyy.MM.dd (E)')
+      })
+
+      return { time }
     }
 }
 </script>
