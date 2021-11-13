944a9f2737711fc5bf3b32724113637242199102
Ayakie <ayalake156@gmail.com>
2021-11-13 18:46:44 +0900

    [add] SingleTask: タスクをコンポーネント化

---
+++ src/components/task/SingleTask.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="task">
+      <p>{{ doc.createdAt }}</p>
+      <h4>{{ doc.userName }}</h4>
+      <p>{{ doc.task }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['doc'],
+    setup(props, context) {
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,36 @@
   <HeroBefore />
   <section class="task">
     <h2>やることリスト</h2>
+    <div class="tasks">
+      <div v-if="documents">
+        <div v-for="doc in documents" :key="doc.id">
+          <SingleTask :doc="doc"/>
+        </div>
+      </div>
+    </div>
     <NewTaskForm />
   </section>
 </template>
 
 <script>
+import getCollection from '@/composables/getCollection'
 import HeroBefore from '@/components/hero/HeroBefore.vue'
 import NewTaskForm from '@/components/task/NewTaskForm.vue'
+import SingleTask from '@/components/task/SingleTask.vue'
 
 export default {
   name: 'Home',
-  components: { HeroBefore, NewTaskForm },
+  components: { HeroBefore, NewTaskForm, SingleTask },
+  setup (){
+    const { error, documents } = getCollection('tasks')
 
+    return { error, documents }
+  }
 }
 </script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+}
+</style>
