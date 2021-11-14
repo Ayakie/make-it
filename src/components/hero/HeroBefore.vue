6637ca908f72077a0ecadc8c660db0e7883013d9
Ayakie <ayalake156@gmail.com>
2021-11-14 14:04:33 +0900

    [update] Heroパーツ画像のスタイリング

--- src/components/hero/HeroBefore.vue
+++ src/components/hero/HeroBefore.vue
@@ -1,22 +1,20 @@
 <template>
     <section class="hero">
-        <Navbar />
+        <div class="hero-container">
         <div class="hero-img">
             <img src="@/assets/setting-goal.png" alt="">
         </div>
-        <div class="hero-text">
-            <h3 class="title">ゴールを達成した自分をイメージしよう</h3>
+        <h3 class="title">ゴールを達成した自分をイメージしよう</h3>
+        <router-link :to="{name: 'SettingGoal'}">
             <button>目標を設定する</button>
+        </router-link>
         </div>
   </section>
 </template>
 
 <script>
-import Navbar from './Navbar.vue'
 
 export default {
-  components: { Navbar },
-
 }
 </script>
 
@@ -24,22 +22,31 @@ export default {
 .hero {
     height: 480px;
     position: relative;
+    /* background-image: url('../../assets/setting-goal.png');
+    background-position: 20%;
+    background-repeat: no-repeat; */
 }
 .hero-container {
+    display: grid;
+    justify-items: center;
+    align-content: center;
     height: 100%;
 }
 .hero-img {
   position: absolute;
-  left: 240px;
-  top: 120px;
+  left: max(15vw, 120px);
+  top: 10%;
+  z-index: -1;
 }
-.hero-text {
-    display: grid;
-    justify-items: center;
-    align-content: center;
-    margin-top: 120px;
-}
-.hero-text .title {
+.title {
     margin-bottom: 24px;
+}
+
+/* smartphone */
+@media (max-width: 768px) {
+    .hero-img {
+        left: 20px;
+        filter: opacity(70%);
+    }
 }
 </style>
