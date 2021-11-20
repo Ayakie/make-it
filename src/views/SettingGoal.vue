0bd6199cb5083b5801c24e9fd3fac76cb2596f53
Ayakie <ayalake156@gmail.com>
2021-11-20 12:37:23 +0900

    [add] handleSubmit data

--- src/views/SettingGoal.vue
+++ src/views/SettingGoal.vue
@@ -4,9 +4,10 @@
       <p>登録することで、達成日までのカウントダウンが表示されます</p>
       <form @submit.prevent="handleSubmit">
           <label for="goal">目標</label>
-          <input type="text" id="goal" required placeholder="例) 〇〇大学に合格する">
+          <input type="text" id="goal" v-model="newGoal" required placeholder="例) 〇〇大学に合格する">
           <label for="goal-date">達成日</label>
-          <input type="date" id="goal-date" required>
+          <input type="date" id="goal-date" v-model="goalDate" required>
+          <div class="error" v-if="error"> {{ error }}</div>
           <button>登録する</button>
       </form>
       <BackPage />
@@ -15,18 +16,41 @@
 
 <script>
 import { useRouter } from 'vue-router'
+import useCollection from '@/composables/useCollection'
+import getUser from '@/composables/getUser'
 import BackPage from '@/components/BackPage.vue'
+import { ref } from '@vue/reactivity'
+import { timestamp } from '@/firebase/config'
 
 export default {
     components: { BackPage },
-    setup() {
+    setup(props, context) {
         const router = useRouter()
+        const newGoal = ref('')
+        const goalDate = ref('')
+        const user = getUser()
+        console.log('user in SettingGoal:' , user.value)
 
-        const handleSubmit = () => {
+        const { error, _addDoc } = useCollection('goals')
 
+        const handleSubmit = async () => {
+            const goal = {
+                userId: user.value.uid,
+                userName: user.value.displayName,
+                goal: newGoal.value,
+                date: goalDate.value,
+                createdAt: timestamp()
+            }
+            if (newGoal.value.length) {
+                const docRef = await _addDoc(goal)
+            }
+            if (!error.value) {
+                newGoal.value =''
+            }
+            router.push({name: 'Home'})
         }
 
-        return { handleSubmit }
+        return {newGoal, goalDate, handleSubmit, error }
     }
 }
 </script>
