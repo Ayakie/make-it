413313251de6d410304678dfa3ca604a95aa7d48
Ayakie <ayalake156@gmail.com>
2021-11-21 14:59:29 +0900

    [update] use DatePicker

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -3,11 +3,20 @@
   <div class="detail" v-if="document">
     <h3>やったことを振り返ろう</h3>
     <form @submit.prevent="handleSubmit">
-      <h4>
+      <h4 class="task-name">
         {{ document.task }}
       </h4>
       <label for="complete-date">完了日</label>
-      <input type="date" id="complete-date" value="2022/11/01">
+      <DatePicker mode="date" id="complete-date" v-model="date" :masks="masks">
+        <template v-slot="{ inputValue, inputEvents }">
+          <input
+          id="complete-date"
+          :value="inputValue"
+          v-on="inputEvents"
+          required
+          >
+        </template>
+      </DatePicker>
       <label for="memo">メモ</label>
       <textarea id="memo"></textarea>
       <label for="">タグ (カテゴリ)</label>
@@ -22,22 +31,36 @@
 import Navbar from "@/components/Navbar.vue"
 import BackPage from '@/components/BackPage.vue'
 import getDocument from "@/composables/getDocument"
+import { ref } from '@vue/reactivity'
+import { DatePicker } from 'v-calendar'
 
 export default {
-  components: { Navbar, BackPage },
+  components: { Navbar, BackPage, DatePicker },
   props: ['id'],
   setup(props) {
     console.log('props', props.id)
     const { error, document, _getDoc } = getDocument('tasks', props.id)
     _getDoc()
 
-    console.log('document in Detail.vue', document)
+    let today = new Date().getDate()
+    const date = ref(new Date())
 
     return { error, document }
+  },
+  data() {
+    return {
+      date: new Date(),
+      masks: {
+        input: 'YYYY/MM/DD'
+      }
+    }
   }
 }
 </script>
 <style scoped>
+.task-name {
+  margin-bottom: 24px;
+}
 .detail {
   text-align: center;
 }
